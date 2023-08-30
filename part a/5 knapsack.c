ee05bae3f49f18c1e4fcd7c3666b563324374ae5
aswarth <m.aswarthreddy@gmail.com>
2023-08-30 12:27:58 +0530

    idk

--- part a/5 knapsack.c
+++ part a/5 knapsack.c
@@ -1,93 +1,70 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-int MFKnapsack(int n, int weights[10], int values[10], int W, int knapsack[100][100])
-{
-    int value;
+int n, W, w [10], v[10], V[10][10], x[10];
 
-    if (knapsack[n][W] < 0)
-    {
-        if (W < weights[n - 1])
-        {
-            value = MFKnapsack(n - 1, weights, values, W, knapsack);
-        }
-        else
-        {
-            int value1 = MFKnapsack(n - 1, weights, values, W, knapsack);
-            int value2 = MFKnapsack(n - 1, weights, values, W - weights[n - 1], knapsack) + values[n - 1];
-            value = (value1 > value2) ? value1 : value2;
-        }
-        knapsack[n][W] = value;
+int max(int a, int b){
+    if(a>b){
+        return a;
     }
-    return knapsack[n][W];
+    else{
+        return b;
+    }
 }
 
-void main()
-{
-    int n, weights[10], values[10], W, knapsack[100][100], ans;
+void print(){
+    int i, j;
+    i = n, j = W;
 
-    printf("Enter the number of Elements\n");
+    while(i != 0 && j != 0){
+        if(V[i][j] != V[i-1][j]){
+            x[i] = 1;
+            j = j - w[i];
+        }
+        i = i-1;
+    }
+
+}
+
+int main(){
+    int i, j;
+
+    printf("Read no of objects\n");
     scanf("%d", &n);
-    printf("Enter the weights of all the elements\n");
 
-    for (int i = 0; i < n; i++)
-    {
-        scanf("%d", &weights[i]);
+    printf("Read weights of objects\n");
+    for(i = 1; i <= n; i++){
+        scanf("%d", &w[i]);
     }
 
-    printf("Enter the values of all the elements\n");
-
-    for (int i = 0; i < n; i++)
-    {
-        scanf("%d", &values[i]);
+    printf("Read the profits\n");
+    for(i = 1; i <= n; i++){
+        scanf("%d", &v[i]);
     }
 
-    printf("Enter the maximum capacity of the Knapsack\n");
-
+    printf("Read knapsack capacity\n");
     scanf("%d", &W);
 
-    for (int i = 0; i <= n; i++)
-    {
-        for (int j = 0; j <= W; j++)
-        {
-            if (i == 0 || j == 0)
-            {
-                knapsack[i][j] = 0;
+    for(i = 0; i <= n; i++){
+        for(j = 0; j <= W; j++){
+            if(i == 0 || j == 0){
+                V[i][j] = 0;
             }
-            else
-            {
-                knapsack[i][j] = -1;
+            else if(j - w[i] < 0){
+                V[i][j] = V[i-1][j];
+            }
+            else{
+                V[i][j] = max(V[i-1][j], V[i-1][j - w[i]] + v[i]);
             }
         }
     }
-
-    ans = MFKnapsack(n, weights, values, W, knapsack);
-
-    for (int i = 0; i <= n; i++)
-    {
-        for (int j = 0; j <= W; j++)
-        {
-            printf("%d\t", knapsack[i][j]);
-        }
-        printf("\n");
-    }
-
-    int i = n;
-    int j = W;
-
-    while (knapsack[i][j] > 0)
-    {
-        if (knapsack[i][j] != knapsack[i - 1][j])
-        {
-            printf("%d is included\n", i);
-            j = j - weights[i - 1];
-            i = i - 1;
-        }
-        else
-        {
-            i = i - 1;
+    print();
+    printf("Max profit is %d\n", V[n][W]);
+    printf("Objects inclded are \n");
+    for(i = 1; i <= n; i++){
+        if(x[i] == 1){
+            printf("%d object is included\n", i);
         }
     }
 
-    printf("The maximum capacity possible is:%d\n", ans);
 }
